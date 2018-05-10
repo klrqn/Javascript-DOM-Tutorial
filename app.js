c2edc02bdb7bd4129c6b12ad779387f09e1e8761
klrqn <burtonsb@gmail.com>
2018-05-10 01:32:28 +0000

    v5

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wmf = document.querySelector("#book-list li:nth-child(2) .name").innerHTML;
-//console.log(wmf);
-
 var books = document.querySelectorAll('#book-list li .name');
-// console.log(book);
 
-var book = document.querySelector('#book-list li .name');
-// console.log(book);
-
-Array.from(books).forEach(function(book){
-	console.log(book);
+Array.from(books).forEach(function(book) {
+	book.textContent += ' (book title)';
 });
+
+const bookList = document.querySelector("#book-list");
+// bookList.innerHTML = '<h2> books and more books </h2>';
+bookList.innerHTML += '<p> this is how you add html';
