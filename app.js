19f2381ceeeb8b1295453c53558cc1767c430dfa
klrqn <burtonsb@gmail.com>
2018-05-10 00:16:30 +0000

    start of really v4

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var titles = document.getElementsByClassName('title');
+// // actually v3
+
+// var titles = document.getElementsByClassName('title');
 
 
-// titles is an "HTMLCollection" see __proto__ .
-console.log(Array.isArray(titles));
-// to turn it into an array use Array.from()
-console.log(Array.isArray(Array.from(titles)));
+// // titles is an "HTMLCollection" see __proto__ .
+// console.log(Array.isArray(titles));
+// // to turn it into an array use Array.from()
+// console.log(Array.isArray(Array.from(titles)));
 
-// turn HTMLCollection into an array and run the forEach method on it
-Array.from(titles).forEach(function(item){
-	console.log(item);
-});
+// // turn HTMLCollection into an array and run the forEach method on it
+// Array.from(titles).forEach(function(item){
+// 	console.log(item);
+// });
+
+
+// v4
+
+const wmf = document.querySelector("#book-list li:nth-child(2) .name").innerHTML;
+console.log(wmf);
