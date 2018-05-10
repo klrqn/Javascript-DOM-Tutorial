a622000500aca15caf935856d47a7d59aa1c01b5
klrqn <burtonsb@gmail.com>
2018-05-10 02:33:30 +0000

    v7

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const banner = document.querySelector("#page-banner");
-
-console.log(banner);
-console.log(banner.textContent);
-
-console.log('#page-banner node type is: ' + banner.nodeType);
-console.log('#page-banner node name is: ' + banner.nodeName);
-console.log('#page-banner has child nodes: ' + banner.hasChildNodes()); // boolean
-
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+const booklist = document.querySelector("#book-list");
 
 
+// traverse up
+console.log('the parent node is: ', booklist.parentNode);
+console.log('the parent element is: ', booklist.parentElement.parentElement);
+
+
+console.log(booklist.childNodes);
+// this gets all child Elements, not all childnodes, which includes all \n returns
+// between elements. so use .children ..
+console.log(booklist.children);
