c907d036ad17c0112e44863e65b1370a3106fb33
klrqn <burtonsb@gmail.com>
2018-05-10 00:21:19 +0000

    fin v4

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-// // actually v3
-
-// var titles = document.getElementsByClassName('title');
-
-
-// // titles is an "HTMLCollection" see __proto__ .
-// console.log(Array.isArray(titles));
-// // to turn it into an array use Array.from()
-// console.log(Array.isArray(Array.from(titles)));
-
-// // turn HTMLCollection into an array and run the forEach method on it
-// Array.from(titles).forEach(function(item){
-// 	console.log(item);
-// });
-
-
-// v4
-
 const wmf = document.querySelector("#book-list li:nth-child(2) .name").innerHTML;
-console.log(wmf);
+//console.log(wmf);
+
+var books = document.querySelectorAll('#book-list li .name');
+// console.log(book);
+
+var book = document.querySelector('#book-list li .name');
+// console.log(book);
+
+Array.from(books).forEach(function(book){
+	console.log(book);
+});
