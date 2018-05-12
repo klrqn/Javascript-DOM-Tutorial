f2c841fa4bb022da119248185199ce472742db25
klrqn <burtonsb@gmail.com>
2018-05-12 13:10:36 +0000

    v16

--- app.js
+++ app.js
@@ -56,3 +56,22 @@ hidebox.addEventListener('change', function(e) {
 	else
 		list.style.display = "initial";
 });
+
+
+// searchbar
+const searchBar = document.forms['search-books'].querySelector('input');
+
+searchBar.addEventListener('keyup', function(e) {
+	const term = e.target.value.toLowerCase();
+	const books = list.getElementsByTagName('li');
+	Array.from(books).forEach(function(book) {
+		const title = book.firstElementChild.textContent;
+
+		if (title.toLowerCase().indexOf(term) != -1 )	{
+			book.style.display = "block";
+		} else {
+			book.style.display = "none";
+		}
+
+	});
+});
